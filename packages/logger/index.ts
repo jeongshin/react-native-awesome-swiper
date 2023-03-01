3946854eab483f7f15cd87a7318d6fe340e20ad0
Jerry <sjeong1127@gmail.com>
2023-03-01 12:45:07 +0900

    chore: replace legacy code

--- packages/logger/index.ts
+++ packages/logger/index.ts
@@ -2,13 +2,13 @@ class Logger {
   public log(...args: any) {
     if (!__DEV__) return;
     // eslint-disable-next-line no-console
-    console.log('[react-native-global-components]', ...args);
+    console.log('[react-native-awesome-swiper]', ...args);
   }
 
   public warn(...args: any) {
     if (!__DEV__) return;
     // eslint-disable-next-line no-console
-    console.warn('[react-native-global-components]', ...args);
+    console.warn('[react-native-awesome-swiper]', ...args);
   }
 }
 
