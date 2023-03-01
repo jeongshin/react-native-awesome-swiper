a197fe7fa43cac3634086c0e3ec04d2c321417a5
Jerry <sjeong1127@gmail.com>
2023-03-01 14:27:03 +0900

    chore: fix docs build config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,17 +11,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://jeongshin.github.io/',
+  url: 'https://react-native-awesome-swiper.site/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'JeongShin', // Usually your GitHub org/user name.
-  projectName: 'jeongshin.github.io', // Usually your repo name.
-  trailingSlash: false,
-  deploymentBranch: 'gh-pages',
+  // organizationName: 'JeongShin', // Usually your GitHub org/user name.
+  // projectName: 'react-native-awesome-swiper', // Usually your repo name.
+  // trailingSlash: false,
+  // deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
